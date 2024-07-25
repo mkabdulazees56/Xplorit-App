ddceafbe021b440cfa637344e404a7ea4bd8ec23
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-07-25 12:58:01 +0500

    updated api key

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -2,5 +2,5 @@
 /* -- LIST OF Constants used in APIs -- */
 
 // Example
-const String tSecretAPIKey = "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
-const String GOOGLE_MAPS_API_KEY = "AIzaSyBOxY_BbJdnSqw8MFTQiM89XJhZCvYUjKs";
+const String tSecretAPIKey = "Add secret key";
+const String GOOGLE_MAPS_API_KEY = "add api key here";
